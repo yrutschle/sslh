yrutschle/sslh

commit 7bf3e12c30d0585743792982ed8bcfc44aecae34

Don't clobber data in libconfig space, copy it before changing it. So far it worked, but really that's not respecting the contract.

common.c
@@ -450,18 +450,24 @@ char* sprintaddr(char* buf, size_t size, struct addrinfo *a)
 
 /* Turns a hostname and port (or service) into a list of struct addrinfo
  * returns 0 on success, -1 otherwise and logs error
- *
- * *host gets modified
- **/
-int resolve_split_name(struct addrinfo **out, char* host, const char* serv)
+ */
+int resolve_split_name(struct addrinfo **out, const char* ct_host, const char* serv)
 {
    struct addrinfo hint;
    char *end;
    int res;
+   char* host;
 
    memset(&hint, 0, sizeof(hint));
    hint.ai_family = PF_UNSPEC;
    hint.ai_socktype = SOCK_STREAM;
+
+   /* Copy parameter so not to clobber data in libconfig */
+   res = asprintf(&host, "%s", ct_host);
+   if (res == -1) {
+       log_message(LOG_ERR, "asprintf: cannot allocate memory");
+       return -1;
+   }
 
    /* If it is a RFC-Compliant IPv6 address ("[1234::12]:443"), remove brackets
     * around IP address */
@@ -474,10 +480,10 @@ int resolve_split_name(struct addrinfo **out, char* host, const char* serv)
        *end = 0; /* remove last bracket */
    }
 
-
    res = getaddrinfo(host, serv, &hint, out);
    if (res)
       log_message(LOG_ERR, "%s `%s:%s'\n", gai_strerror(res), host, serv);
+   free(host);
    return res;
 }
 

common.h
@@ -106,7 +106,7 @@ void drop_privileges(const char* user_name, const char* chroot_path);
 void write_pid_file(const char* pidfile);
 void log_message(int type, char* msg, ...);
 void dump_connection(struct connection *cnx);
-int resolve_split_name(struct addrinfo **out, char* hostname, const char* port);
+int resolve_split_name(struct addrinfo **out, const char* hostname, const char* port);
 
 int start_listen_sockets(int *sockfd[], struct addrinfo *addr_list);
 

commit 1a6ba5edc0b4482182ec6603433435ff091f66b6

fix IPv6 parse error introduced in 7bf3e12c30d0585743792982ed8bcfc44aecae34

common.c
@@ -456,18 +456,20 @@ int resolve_split_name(struct addrinfo **out, const char* ct_host, const char* s
    struct addrinfo hint;
    char *end;
    int res;
-   char* host;
+   char* host, *host_base;
 
    memset(&hint, 0, sizeof(hint));
    hint.ai_family = PF_UNSPEC;
    hint.ai_socktype = SOCK_STREAM;
 
    /* Copy parameter so not to clobber data in libconfig */
-   res = asprintf(&host, "%s", ct_host);
+   res = asprintf(&host_base, "%s", ct_host);
    if (res == -1) {
        log_message(LOG_ERR, "asprintf: cannot allocate memory");
        return -1;
    }
+
+   host = host_base;
 
    /* If it is a RFC-Compliant IPv6 address ("[1234::12]:443"), remove brackets
     * around IP address */
@@ -483,7 +485,7 @@ int resolve_split_name(struct addrinfo **out, const char* ct_host, const char* s
    res = getaddrinfo(host, serv, &hint, out);
    if (res)
       log_message(LOG_ERR, "%s `%s:%s'\n", gai_strerror(res), host, serv);
-   free(host);
+   free(host_base);
    return res;
 }
 