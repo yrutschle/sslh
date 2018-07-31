68cd18ca915af97b7896de357c1b6ff9ca505f81
Aleksei Faians <af.git@bsrealm.net>
2018-07-31 04:07:37 +0300

    allow more regexp probe attempts

--- probe.c
+++ probe.c
@@ -355,7 +355,7 @@ static int regex_probe(const char *p, int len, struct proto *proto)
     for (; *probe && regexec(*probe, p, 0, &pos, REG_STARTEND); probe++)
         /* try them all */;
 
-    if (*probe == NULL && len < 5)
+    if (*probe == NULL && len < 15)
         return PROBE_AGAIN;
 
     return (*probe != NULL);
