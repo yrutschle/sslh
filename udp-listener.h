e884fc616d5d1049e9fcdd544159e425add3697a
yrutschle <git1@rutschle.net>
2021-03-03 18:20:45 +0100

    add conditional inclusion for header

--- udp-listener.h
+++ udp-listener.h
@@ -1,5 +1,11 @@
+#ifndef UDPLISTENER_H
+#define UDPLISTENER_H
 
 /* UDP listener: upon incoming packet, find where it should go
  * This is run in its own process and never returns.
  */
 void udp_listener(struct listen_endpoint* endpoint, int num_endpoints, int active_endpoint);
+
+
+
+#endif /* UDPLISTENER_H */
